02b0be564413b0996c59eb100bb73fcd42f4c599
DRUNK01 <105923004+DRUNK01@users.noreply.github.com>
2022-11-15 01:38:16 +0600

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -656,9 +656,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "100087599954638",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "17%3AzGUstvRirS0J6A%3A2%3A1668454230%3A-1%3A-1"
 }
 
 
